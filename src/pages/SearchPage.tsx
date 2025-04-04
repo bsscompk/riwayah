fbf382794de1563226aee4d1a824dbbf0031b5d2
Mansoor Ahmed Memon <mansoormemon@bss.com.pk>
2025-04-04 21:55:38 +0500

    feature(ui): design filters component

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -46,6 +46,81 @@ const HadithDetail = (props: { data: any }) => {
     );
 };
 
+const HadithFilters = () => {
+    return (
+        <div class="max-w-96 flex-grow flex flex-col gap-1">
+            <h1 class="font-bold text-lg">Filters</h1>
+            <form action="/search" class="w-full flex flex-col gap-4 py-4 rounded-2xl">
+                <div class="flex flex-col gap-1">
+                    <label for="hadithNumber" class="pl-1 text-xs">
+                        Hadith Number
+                    </label>
+                    <input
+                        id="hadithNumber"
+                        name="hadithNumber"
+                        type="number"
+                        class="w-full px-4 py-2 rounded-2xl focus:outline-none border border-neutral-300 hover:border-neutral-400 focus:border-amber-500 transition-all"
+                        placeholder="Enter Hadith Number"
+                    />
+                </div>
+                <div class="flex flex-col gap-1">
+                    <label for="book" class="pl-1 text-xs">
+                        Book
+                    </label>
+                    <select
+                        id="book"
+                        name="book"
+                        class="w-full px-4 py-2 rounded-2xl focus:outline-none border border-neutral-300 hover:border-neutral-400 focus:border-amber-500 transition-all">
+                        <option value="">Select Book</option>
+                        <option value="sahih-bukhari">Sahih Bukhari</option>
+                        <option value="sahih-muslim">Sahih Muslim</option>
+                        <option value="al-tirmidhi">Jami' Al-Tirmidhi</option>
+                        <option value="abu-dawood">Sunan Abu Dawood</option>
+                        <option value="ibn-e-majah">Sunan Ibn-e-Majah</option>
+                        <option value="sunan-nasai">Sunan An-Nasa`i</option>
+                        <option value="mishkat">Mishkat Al-Masabih</option>
+                        <option value="musnad-ahmad">Musnad Ahmad</option>
+                        <option value="al-silsila-sahiha">Al-Silsila Sahiha</option>
+                    </select>
+                </div>
+                <div class="flex flex-col gap-1">
+                    <label for="chapter" class="pl-1 text-xs">
+                        Chapter
+                    </label>
+                    <input
+                        id="chapter"
+                        name="chapter"
+                        type="number"
+                        class="w-full px-4 py-2 rounded-2xl focus:outline-none border border-neutral-300 hover:border-neutral-400 focus:border-amber-500 transition-all"
+                        placeholder="Enter Chapter Number"
+                    />
+                </div>
+                <div class="flex flex-col gap-1">
+                    <label for="status" class="pl-1 text-xs">
+                        Status
+                    </label>
+                    <select
+                        id="status"
+                        name="status"
+                        class="w-full px-4 py-2 rounded-2xl focus:outline-none border border-neutral-300 hover:border-neutral-400 focus:border-amber-500 transition-all">
+                        <option value="">Select Status</option>
+                        <option value="Sahih">Sahih</option>
+                        <option value="Hasan">Hasan</option>
+                        <option value="Da`eef">Da`eef</option>
+                    </select>
+                </div>
+                <div class="flex justify-end">
+                    <button
+                        type="submit"
+                        class="px-4 py-2 rounded-2xl bg-amber-500 hover:bg-amber-600 active:bg-amber-700 text-white transition-all">
+                        Apply Filters
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
 const SearchPage = () => {
     const [results] = createStore(STATIC_RESPONSE);
 
@@ -60,14 +135,19 @@ const SearchPage = () => {
                         <SearchBar />
                     </div>
                 </div>
-                <div class="max-w-7xl flex flex-col gap-2 px-4 md:px-24 lg:px-48 xl:px-64 py-2 transition-all">
-                    <div>
-                        <p>
-                            Hadiths Found: <b>{results.length}</b>
-                        </p>
+                <div class="flex gap-8 px-4 md:px-8 xl:px-16 2xl:px-32 py-2 transition-all">
+                    <div class="max-w-4xl w-full lg:w-2/3 flex flex-col gap-2">
+                        <div>
+                            <p>
+                                Hadiths Found: <b>{results.length}</b>
+                            </p>
+                        </div>
+                        <div class="flex flex-col">
+                            <For each={results}>{(item) => <HadithDetail data={item} />}</For>
+                        </div>
                     </div>
-                    <div class="flex flex-col">
-                        <For each={results}>{(item, index) => <HadithDetail data={item} />}</For>
+                    <div class="flex-grow hidden lg:flex justify-start border-l border-neutral-200 pl-8">
+                        <HadithFilters />
                     </div>
                 </div>
             </main>
